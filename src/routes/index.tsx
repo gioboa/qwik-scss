569e2d2eb71cfb801bdf2e51c0cc27b7ef52b41a
Luis Monsalve <luismonsalve1941@gmail.com>
2023-05-09 21:51:25 -0400

    :bug: bug: scss modules in awik

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import './../global/style/main.scss'
 
 export default component$(() => {
   return (
